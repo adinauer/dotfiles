e0306fd16733f57a2ab6d54faec9de51bb280e50
Alexander Dinauer <alexander@dinauer.at>
2015-08-15 11:38:03 +0000

    Add completion for docker and docker-compose.

--- home/.zsh/adinauer/_other.zsh
+++ home/.zsh/adinauer/_other.zsh
@@ -1,4 +1,6 @@
 source ~/.zsh/adinauer/commands/z.completion
+source ~/.zsh/adinauer/commands/docker.completion
+source ~/.zsh/adinauer/commands/docker-compose.completion
 
 export WORDCHARS='*?[]~&;!$%^<>'
 export LESS=-rFX
